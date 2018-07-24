97df4c4a7e19dac25c0eeec56a3143c482ab9051
Fernando Mayer <fernandomayer@gmail.com>
2018-07-24 20:19:36 -0300

    Muda configurações de deploy

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "fernandomayer@gmail.com"
-git config --global user.name "Fernando Mayer"
+git config --global user.email "leg.ufpr@gmail.com"
+git config --global user.name "LEG UFPR"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../docs/* ./
 git add --all *
-git commit -m "Update the book" || true
+git commit -m "The book is on the repo." || true
 git push -q origin gh-pages
